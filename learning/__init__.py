a58fda1a1de83b20839a0652a1aacbccfe19a532
Sergey Sorokumov <ssorokumov@gmail.com>
2022-10-14 13:36:53 +0300

    delete previos files

--- learning/__init__.py
+++
@@ -1,4 +0,0 @@
-from .Dataset import Dataset
-from .batch import BatchGenerator
-from .ParamsNet import ParamsNet
-from .trainer import Trainer
--- learning/main.py
+++
@@ -1,8 +0,0 @@
-#from learning import Dataset
-from learning import Trainer
-
-
-if __name__ == "__main__":
-    trainer = Trainer('model_with_real', 128, 2)
-    trainer.train(300000)
-    #trainer.evaluate('epoch_epoch_650')
